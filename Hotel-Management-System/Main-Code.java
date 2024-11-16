3a155ca025ac797a7a7ed36ae7f83f100006bc8f
marwan-mar3y <160648176+marwan-mar3y@users.noreply.github.com>
2024-11-16 21:41:21 +0200

    Create Main-Code.java

---
+++ Hotel-Management-System/Main-Code.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hotelmain;
+
+import java.util.Scanner;
+public class Hotelmain {
+ public static final Scanner input = new Scanner(System.in);
+    public static final String AVAILABLE = "available";
+    public static final String OCCUPIED = "occupied";
+    private static final Hotel hotel = new Hotel();
+
+    public static void main(String[] args) {
+        hotel.start();}}
