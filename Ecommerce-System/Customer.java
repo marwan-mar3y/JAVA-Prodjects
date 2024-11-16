da27a5c8611c9e6ea316cacb8f4117f1b7c2ee2c
marwan-mar3y <160648176+marwan-mar3y@users.noreply.github.com>
2024-11-16 16:38:19 +0200

    Create Customer.java

---
+++ Ecommerce-System/Customer.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ecommercesystem;
+
+/**
+ *
+ * @author mar3y
+ */
+public class Customer {
+ private int customerid;
+ private String name;
+ private String address;
+
+ public Customer(int customerid,String name,String address){
+    this.customerid=customerid;
+    this.name=name;
+    this.address=address;
+ }
+ 
+    public int getCustomerid() {
+        return customerid;
+    }
+
+    public void setCustomerid(int customerid) {
+        this.customerid = customerid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+ 
+}
